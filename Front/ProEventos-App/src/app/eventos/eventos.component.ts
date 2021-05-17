a1d271f0616b40b4905a0fcae454e385301ff63a
Suzana Rozisca <srozisca@gmail.com>
2021-05-16 21:22:56 -0300

    adição e configuração do angular

---
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent implements OnInit {
+
+  public eventos: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      response => this.eventos = response,
+      error => console.log(error)
+    );
+
+    // this.eventos =
+    // [
+    //     {
+    //       Tema: 'Angular 11',
+    //       Local: 'Rio de Janeiro'
+    //     },
+    //     {
+    //       Tema: '.NET',
+    //       Local: 'São Paulo'
+    //     },
+    //     {
+    //       Tema: 'Angular e suas novidades',
+    //       Local: 'Belo Horizonte'
+    //     }
+    // ]
+
+  }
+}
